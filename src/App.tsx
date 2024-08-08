e01f85fff34759281dfbd7b283df66245e382b42
Akbarshox <akbarshox@MacBook-Air-Akbarshox.local>
2024-08-08 11:27:46 +0500

    ozgardi

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,7 @@ const App = () => {
     <Router>
       <div className="flex">
         <Sidebar />
-        <div className="flex-1 p-6">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/cost" element={<Cost />} />
-            <Route path="/appliances" element={<Appliances />} />
-          </Routes>
-        </div>
+        
       </div>
     </Router>
   );
