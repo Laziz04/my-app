c10cac9fc0413edfa7f3e89166de429ff93fdbf5
Abdushukurov Abdulaziz <frontendgithub@gmail.com>
2024-08-08 13:43:58 +0500

    .

--- src/pages/oqituvchilar.tsx
+++ src/pages/oqituvchilar.tsx
@@ -15,90 +15,6 @@ interface DataType {
   phone: string;
 }
 
-// // `dataSource` ma'lumotlari
-// const dataSource: DataType[] = [
-//   {
-//     key: 1,
-//     firstName: "Sarvar",
-//     lastName: "Akramov",
-//     subject: "Matematika",
-//     email: "sarvar.akramov@example.com",
-//     phone: "+998901234567",
-//   },
-//   {
-//     key: 2,
-//     firstName: "Dilnoza",
-//     lastName: "Karimova",
-//     subject: "Ingliz tili",
-//     email: "dilnoza.karimova@example.com",
-//     phone: "+998901234568",
-//   },
-//   {
-//     key: 3,
-//     firstName: "Abdulloh",
-//     lastName: "Raxmatullayev",
-//     subject: "Fizika",
-//     email: "abdulloh.raxmatullayev@example.com",
-//     phone: "+998901234569",
-//   },
-//   {
-//     key: 4,
-//     firstName: "Gulbahor",
-//     lastName: "Sobirova",
-//     subject: "Kimyo",
-//     email: "gulbahor.sobirova@example.com",
-//     phone: "+998901234570",
-//   },
-//   {
-//     key: 5,
-//     firstName: "Bahrom",
-//     lastName: "Tursunov",
-//     subject: "Tarix",
-//     email: "bahrom.tursunov@example.com",
-//     phone: "+998901234571",
-//   },
-//   {
-//     key: 6,
-//     firstName: "Nigora",
-//     lastName: "Mirzaeva",
-//     subject: "Biologiya",
-//     email: "nigora.mirzaeva@example.com",
-//     phone: "+998901234572",
-//   },
-//   {
-//     key: 7,
-//     firstName: "Ulug'bek",
-//     lastName: "Islomov",
-//     subject: "Geografiya",
-//     email: "ulugbek.islomov@example.com",
-//     phone: "+998901234573",
-//   },
-//   {
-//     key: 8,
-//     firstName: "Zulfiya",
-//     lastName: "Abdukarimova",
-//     subject: "Adabiyot",
-//     email: "zulfiya.abdukarimova@example.com",
-//     phone: "+998901234574",
-//   },
-//   {
-//     key: 9,
-//     firstName: "Jasur",
-//     lastName: "Nurmatov",
-//     subject: "Informatika",
-//     email: "jasur.nurmatov@example.com",
-//     phone: "+998901234575",
-//   },
-//   {
-//     key: 10,
-//     firstName: "Dilshoda",
-//     lastName: "Saidova",
-//     subject: "Rus tili",
-//     email: "dilshoda.saidova@example.com",
-//     phone: "+998901234576",
-//   },
-// ];
-
 const columns: TableColumnsType<DataType> = [
   { title: "First Name", dataIndex: "firstName", key: "firstName" },
   { title: "Last Name", dataIndex: "lastName", key: "lastName" },
@@ -114,7 +30,6 @@ const Oqituvchilar: React.FC = () => {
 
   const start = () => {
     setLoading(true);
-    // Ajax request after empty completing
     setTimeout(() => {
       setSelectedRowKeys([]);
       setLoading(false);
